0dbd9f10e565fdb6e4ae12b76e5d8cf74007fd94
Paul Friedrich <mail@paulfriedrich.me>
2023-06-25 10:29:03 +0200

    set vm password

--- hosts/e595/configuration.nix
+++ hosts/e595/configuration.nix
@@ -116,6 +116,9 @@
     checkReversePath = "loose"; # for tailscale
   };
 
+  virtualisation.vmVariant = {
+    users.users.pfriedrich.password = "foo";
+  };
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
