9cfc00d04b6f4060a986002a2f8eaa071476cbf0
Paul Friedrich <mail@paulfriedrich.me>
2023-07-17 23:02:17 +0200

    add firewall to vultr gateway

--- infrastructure/vultr-gateway/Program.cs
+++ infrastructure/vultr-gateway/Program.cs
@@ -4,6 +4,66 @@ using Pulumi;
 
 return await Deployment.RunAsync(() =>
 {
+	FirewallGroup firewall = new("firewall", new FirewallGroupArgs
+	{
+		Description = "firewall for the router"
+	});
+
+	// FirewallRule firewallSshV4 = new("firewall-ssh-4", new FirewallRuleArgs
+	// {
+	// 	FirewallGroupId = firewall.Id,
+	// 	Protocol = "tcp",
+	// 	IpType = "v4",
+	// 	Subnet = "0.0.0.0",
+	// 	SubnetSize = 0,
+	// 	Port = "2704",
+	// 	Notes = "Allow SSH"
+	// });
+	//
+	// FirewallRule firewallSshV6 = new("firewall-ssh-6", new FirewallRuleArgs
+	// {
+	// 	FirewallGroupId = firewall.Id,
+	// 	Protocol = "tcp",
+	// 	IpType = "v6",
+	// 	Subnet = "::",
+	// 	SubnetSize = 0,
+	// 	Port = "2704",
+	// 	Notes = "Allow SSH"
+	// });
+
+	FirewallRule firewallWireguardV4 = new("firewall-wireguard-4", new FirewallRuleArgs
+	{
+		FirewallGroupId = firewall.Id,
+		Protocol = "udp",
+		IpType = "v4",
+		Subnet = "0.0.0.0",
+		SubnetSize = 0,
+		Port = "29626",
+		Notes = "Allow Wireguard"
+	});
+
+	FirewallRule firewallWireguardV6 = new("firewall-wireguard-6", new FirewallRuleArgs
+	{
+		FirewallGroupId = firewall.Id,
+		Protocol = "udp",
+		IpType = "v6",
+		Subnet = "::",
+		SubnetSize = 0,
+		Port = "29626",
+		Notes = "Allow Wireguard"
+	});
+
+	FirewallRule firewallBgpV6 = new("firewall-bgp-6", new FirewallRuleArgs
+	{
+		FirewallGroupId = firewall.Id,
+		Protocol = "tcp",
+		IpType = "v6",
+		Subnet = "2001:19f0:ffff::1",
+		SubnetSize = 128,
+		Port = "179",
+		Notes = "Allow BGP"
+	});
+
 	Instance routerVm = new("router", new InstanceArgs
 	{
 		Hostname = "vm-router-prod-fra-01",
@@ -16,7 +76,8 @@ return await Deployment.RunAsync(() =>
 		VpcIds =
 		{
 			"10509d8c-b9b7-40b3-95f4-b7da469bac41" // main
-		}
+		},
+		FirewallGroupId = firewall.Id
 	}, new CustomResourceOptions { Protect = true });
 
 	// Export outputs here
