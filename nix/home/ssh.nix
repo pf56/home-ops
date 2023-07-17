4a1d87c585b0f90cce44456b6bfeec3726e8933b
Paul Friedrich <mail@paulfriedrich.me>
2023-07-17 23:12:21 +0200

    add comment

--- nix/home/ssh.nix
+++ nix/home/ssh.nix
@@ -19,6 +19,7 @@
     };
 
     extraOptionOverrides = {
+      # https://unix.stackexchange.com/questions/280879/how-to-get-pinentry-curses-to-start-on-the-correct-tty/499133#499133
       "Match" = "host * exec \"gpg-connect-agent UPDATESTARTUPTTY /bye\"";
     };
   };
