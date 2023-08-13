ca3c553712c9d06e1990f3072eb54afd339908a0
Paul Friedrich <mail@paulfriedrich.me>
2023-08-13 13:01:18 +0200

    nix: remove unused code

--- nix/home/ssh.nix
+++ nix/home/ssh.nix
@@ -3,20 +3,6 @@
 {
   programs.ssh = {
     enable = true;
-    matchBlocks = {
-      "setKey" = {
-        host = "*";
-        identityFile = "~/.ssh/id_ed25519";
-      };
-
-      "git.internal.paulfriedrich.me" = {
-        host = "git.internal.paulfriedrich.me";
-        extraOptions = {
-          "HostKeyAlgorithms" = "+ssh-rsa";
-          "PubKeyAcceptedKeyTypes" = "+ssh-rsa";
-        };
-      };
-    };
 
     extraOptionOverrides = {
       # https://unix.stackexchange.com/questions/280879/how-to-get-pinentry-curses-to-start-on-the-correct-tty/499133#499133
