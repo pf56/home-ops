6987458ece8c911f02301d7ec4c35b33fbefb6ab
Paul Friedrich <mail@paulfriedrich.me>
2023-11-04 11:32:59 +0100

    nix: add new git ssh key

--- nix/hosts/git/configuration.nix
+++ nix/hosts/git/configuration.nix
@@ -28,6 +28,7 @@
     shell = "${pkgs.git}/bin/git-shell";
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCYzbuUnTGcwuYXXCve+ktgTd4HZRWCZ6j9iP0YJoS0lOMfZHxMkgLmUue/xZeeH2DkwvyVSCjlBuqhh979iVTzaLUQk6VfcryCSqST8DYtYmDPWGtEa9x64EsC1bCk78iQlILTDul11tCdAsfd9y/f4OluhdVZhPBAcoxROU5UTPc8nSrPk1+x8/x7Z0GrPvm8txx3BQyAfcQCQ/caduSu1Gd65BTcpM2FYdT6USlu4oUYvxwILuxd5VxHTpi2yyv19zp3ROx08UOylctPCr7AKnoPsUF59MrLyRmyAr1AuENY6N/NXypYA9ANrimg4ktHCc1ncWO1uMc4yhtBbUIegsZKGXhV+L7WlHKZ93PGnY08CypXH3whG4xUlSaIhuhpy/xIIDJQaKTk8Fljyb8GaD6e1C8W5efE2td4i+kEwCEaHk0/Vh3SyPNLIIW/A1d1EAtOU+kcmReS2VKJx5d/1CYwKxnuN6JyJSzODrgByeYxeaYP9ea2MG78AL5SO2ekmJuQhF6m3Df6NGmeCHKrXBsEchtobF88rATPfxMwnxrOZQYKXARYjqwU8or0qsE+C68uiQBkTFJHJAqF9Eo1QVK34A8jUYrOeY5au9cDFE01W/aY/rNl3UjmfmsvwIrXsLPbCjeq4rqT5L2K60x3gb9DZVruQB8N2vfZVecCpw==" # pfriedrich yubikey
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN0XmUGINQzn6lRxfvhjuxYYpjHFzKPzFaJZ5W8bFAiC" # pfriedrich phone
     ];
   };
 
