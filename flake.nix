ad80ce923a858cce7a810eac84e3167fcb883283
Paul Friedrich <mail@paulfriedrich.me>
2024-04-20 11:14:21 +0200

    nix: upgrade
    back to unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
-    nixpkgs-unstable.url = "nixpkgs/master";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     nixos-wsl.url = github:nix-community/NixOS-WSL/main;
     sops-nix.url = github:Mic92/sops-nix;
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
