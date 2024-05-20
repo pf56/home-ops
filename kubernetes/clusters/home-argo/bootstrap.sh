456e2873172688fae3f8367ea039bc4fe6ec6392
Paul Friedrich <mail@paulfriedrich.me>
2024-05-20 17:25:29 +0200

    k8s: configure helm-secrets

--- kubernetes/clusters/home-argo/bootstrap.sh
+++ kubernetes/clusters/home-argo/bootstrap.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+# restore key into key.txt first (or create a new on `age-keygen -o key.txt`)
+kubectl -n argocd create secret generic helm-secrets-private-keys --from-file=key.txt=key.txt
 kubectl kustomize . --enable-helm | kubectl apply -f -
