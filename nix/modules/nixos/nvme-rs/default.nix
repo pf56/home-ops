08c3d7d32064b19c536391e7bc4b4877a7f34de1
Paul Friedrich <mail@paulfriedrich.me>
2025-12-09 18:45:39 +0100

    nix/nvme-rs: adjust temp thresholds

--- nix/modules/nixos/nvme-rs/default.nix
+++ nix/modules/nixos/nvme-rs/default.nix
@@ -27,15 +27,35 @@ in
       enable = true;
 
       settings = {
-        email = {
-         from = lib.concatStrings ["status" "@" "mail.paulfriedrich.me"];
-         to = lib.concatStrings ["status" "@" "mail.paulfriedrich.me"];
+        thresholds = {
+          temp_warning = 65;
+          temp_critical = 80;
+        };
 
-         smtp_server = "smtp.fastmail.com";
-         smtp_port = 587;
-         smtp_use_tls = true;
-         smtp_username = lib.concatStrings ["mail" "@" "paulfriedrich.me"];
-         smtp_password_file = "/run/credentials/nvme-rs.service/smtpSecret";
+        email = {
+          from = lib.concatStrings [
+            "status"
+            "@"
+            "mail.paulfriedrich.me"
+          ];
+
+          to = lib.concatStrings [
+            "status"
+            "@"
+            "mail.paulfriedrich.me"
+          ];
+
+          smtp_server = "smtp.fastmail.com";
+          smtp_port = 587;
+          smtp_use_tls = true;
+
+          smtp_username = lib.concatStrings [
+            "mail"
+            "@"
+            "paulfriedrich.me"
+          ];
+
+          smtp_password_file = "/run/credentials/nvme-rs.service/smtpSecret";
         };
       };
     };
