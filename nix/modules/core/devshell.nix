bec7f5687c677fe2a1f55d242c7cae1bc0cc09f5
Paul Friedrich <mail@paulfriedrich.me>
2026-05-23 19:04:33 +0200

    nix: set NH_FLAKE

--- nix/modules/core/devshell.nix
+++ nix/modules/core/devshell.nix
@@ -1,12 +1,16 @@
 { ... }:
 {
   perSystem =
-    { config, pkgs, ... }:
+    { pkgs, ... }:
     {
       devShells.default = pkgs.mkShell {
         packages = [
           pkgs.nh
         ];
+
+        shellHook = ''
+          export NH_FLAKE="$(git rev-parse --show-toplevel 2>/dev/null || pwd)"
+        '';
       };
     };
 }
