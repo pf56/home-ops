d683e2898cb37e0dfe15c351c7904f53ccbfd4bb
Paul Friedrich <mail@paulfriedrich.me>
2026-07-21 22:01:19 +0200

    nix/ai: add grafana mcp server

--- nix/modules/dev/ai.nix
+++ nix/modules/dev/ai.nix
@@ -1,15 +1,45 @@
 { lib, ... }:
 {
   den.aspects.ai = {
-    homeManager =
-      { pkgs, ... }:
+    nixos =
+      { user, ... }:
       {
-        home.packages = with pkgs; [
-          opencode
-        ];
+        users.groups.grafana-mcp = { };
+        users.users.${user.userName}.extraGroups = [ "grafana-mcp" ];
+
+        sops.secrets = {
+          "ai/grafana-mcp-token" = {
+            owner = "pfriedrich";
+            group = "grafana-mcp";
+            mode = "0440";
+          };
+        };
+      };
+
+    homeManager =
+      { osConfig, pkgs, ... }:
+      {
+        home.packages = with pkgs; [ ];
+
+        programs.opencode = {
+          enable = true;
+          enableMcpIntegration = true;
+        };
+
+        programs.mcp = {
+          enable = true;
+          servers = {
+            grafana = {
+              command = "${lib.getExe pkgs.mcp-grafana}";
+              env = {
+                GRAFANA_URL = "https://dashboard.internal.paulfriedrich.me";
+                GRAFANA_SERVICE_ACCOUNT_TOKEN.file = osConfig.sops.secrets."ai/grafana-mcp-token".path;
+              };
+            };
+          };
+        };
 
         xdg.configFile."opencode/opencode.json".text = builtins.toJSON {
-          "$schema" = "https://opencode.ai/config.json";
           autoupdate = false;
           share = "disabled";
         };
