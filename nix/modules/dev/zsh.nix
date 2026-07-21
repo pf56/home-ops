31f3f653a89806e7f4c26343efe03cc11247deac
Paul Friedrich <mail@paulfriedrich.me>
2026-07-21 23:01:33 +0200

    nix/zsh: add argocd-local-sync

--- nix/modules/dev/zsh.nix
+++ nix/modules/dev/zsh.nix
@@ -26,7 +26,6 @@
 
           shellAliases = {
             l = "ls -l";
-            argocd-local-diff = "argocd app diff --local \"$(git rev-parse --show-toplevel)\" --server-side-generate";
           };
 
           initContent = ''
@@ -44,6 +43,55 @@
             # ctrl+left/right
             bindkey $terminfo[kLFT5] backward-word
             bindkey $terminfo[kRIT5] forward-word
+
+            argocd-local-diff() {
+              if (( $# < 1 )); then
+                print -u2 "usage: argocd-local-diff <application> [argocd diff flags]"
+                return 2
+              fi
+
+              local app="$1"
+              local directory_name="''${PWD##*/}"
+              local repo_root
+              shift
+
+              if [[ "$directory_name" != "$app" ]]; then
+                print -u2 "refusing to diff application \"$app\" from \"$PWD\""
+                return 2
+              fi
+
+              repo_root="$(git rev-parse --show-toplevel)" || return
+
+              argocd app diff "$app" \
+                --local "$PWD" \
+                --local-repo-root "$repo_root" \
+                --server-side-generate \
+                "$@"
+            }
+
+            argocd-local-sync() {
+              if (( $# < 1 )); then
+                print -u2 "usage: argocd-local-sync <application> [argocd sync flags]"
+                return 2
+              fi
+
+              local app="$1"
+              local directory_name="''${PWD##*/}"
+              local repo_root
+              shift
+
+              if [[ "$directory_name" != "$app" ]]; then
+                print -u2 "refusing to sync application \"$app\" from \"$PWD\""
+                return 2
+              fi
+
+              repo_root="$(git rev-parse --show-toplevel)" || return
+
+              argocd app sync "$app" \
+                --local "$PWD" \
+                --local-repo-root "$repo_root" \
+                "$@"
+            }
           '';
 
           plugins = [
